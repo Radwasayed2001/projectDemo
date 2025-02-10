5b93f41bc7daec4e169ceb166c8a2235c9d36866
Radwasayed2001 <120200917+Radwasayed2001@users.noreply.github.com>
2025-02-10 19:28:19 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,1 @@
-document.querySelector('.ui-wheel-of-fortune button').addEventListener('click', function() {
-  const wheel = document.querySelector('.ui-wheel-of-fortune ul');
-  const segments = 12;
-  const randomIndex = Math.floor(Math.random() * segments);
-  const degrees = 360 * 5 + (360 / segments) * randomIndex; // 5 full rotations + segment rotation
-  console.log(randomIndex);
-  wheel.style.transition = 'transform 4s ease-out';
-  wheel.style.transform = `rotate(${degrees}deg)`;
 
-  setTimeout(() => {
-      Swal.fire({
-          title: 'Congratulations!',
-          text: `You won $${(randomIndex) * 1000}!`,
-          icon: 'success',
-          confirmButtonText: 'Awesome!',
-          confirmButtonColor: 'rgb(254, 44, 85)'
-      });
-  }, 4000); // 4 seconds
-});
